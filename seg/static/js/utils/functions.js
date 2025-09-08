76d4aff65ce1b18a6858a3ee8aa73784cdc32709
obha <obha121@outlook.com>
2025-09-08 14:07:08 +0100

    Remove deprecated command files and associated functionality
    - Deleted command files related to material manipulation (SetMaterialRangeCommand, SetMaterialValueCommand, SetMaterialVectorCommand).
    - Removed commands for setting object properties (SetPositionCommand, SetRotationCommand, SetScaleCommand).
    - Eliminated commands for scene management (SetSceneCommand, SetScriptValueCommand, SetUuidCommand, SetValueCommand).
    - Removed UpdateObjectCommand and index.js for command exports.
    - Cleaned up main.js and utility functions related to Three.js rendering and event handling.
    - Removed test script for graceful shutdown and the entire ThreeJsRenderer class.

--- seg/static/js/utils/functions.js
+++
@@ -1,9 +0,0 @@
-export function debounce(func, delay = 1000) {
-  let debounceTimer;
-  return function () {
-    const context = this;
-    const args = arguments;
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => func.apply(context, args), delay);
-  };
-}
